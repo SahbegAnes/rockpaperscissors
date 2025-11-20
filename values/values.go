c5075e452615c3821e54a487a49dbc6c54cb553d
Anes Sahbegovic <anes.sahbegovic@gmail.com>
2025-11-20 17:07:07 +0100

    values

--- values/values.go
+++ values/values.go
@@ -10,12 +10,28 @@ const (
 
 // String liefert den Wert als String.
 func (v Value) String() string {
-	// TODO
-	return ""
+	switch v {
+	case Rock:
+		return "Stein"
+	case Paper:
+		return "Papier"
+	case Scissors:
+		return "Schere"
+	default:
+		return "Gib was gescheites an"
+	}
 }
 
 // Beats gibt an, ob der Wert v den Wert w schlägt.
 func (v Value) Beats(w Value) bool {
-	// TODO
+	if v == Rock && w == Scissors {
+		return true
+	}
+	if v == Paper && w == Rock {
+		return true
+	}
+	if v == Scissors && w == Paper {
+		return true
+	}
 	return false
 }
